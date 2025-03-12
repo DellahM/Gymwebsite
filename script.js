c5ebf4796b283081a0dd7be80be3dcde51f576eb
KairoTech <dellahmiheso@gmail.com>
2025-03-12 10:21:03 +0300

    Adjustments

---
+++ script.js
@@ -0,0 +1,9 @@
+//Js
+document.querySelector(".mobile-btn").addEventListener("click", function () {
+    document.querySelector(".menu").classList.toggle("active");
+  });
+
+  //Js
+document.querySelector(".mobile-btn").addEventListener("click", function () {
+    document.querySelector(".menu").classList.toggle("active");
+  });
